da89d2b7aeb4e36a9b19df1e7ad92982497fd207
Nugha Darmawan <nugha.darmawan@gmail.com>
2013-12-09 17:43:00 +0700

    Add bootstrap and applied layout to the page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnotherTreebook::Application.config.secret_key_base = 'c29042a8129e4ffd3ed931dc21bc65c45121ce9d8264d0c98d510914944b13bde086f6caa746d44786d905ed5640b13bb09fb6daa599946cf8c1d6dd7bfaa50f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnotherTreebook::Application.config.session_store :cookie_store, key: '_AnotherTreebook_session'
